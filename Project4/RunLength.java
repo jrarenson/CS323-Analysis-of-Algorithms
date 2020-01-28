d8b7b37fc25b1b7b27c6cb5066fe07ee41b18257
jrarenson <jonathan.arenson44@gmail.com>
2020-01-28 14:54:43 -0500

    Added Run Length Java file

---
+++ Project4/RunLength.java
@@ -0,0 +1,25 @@
+/*
+*
+*Jonathan Arenson
+*CS323-21
+*
+*/
+
+public class RunLength{
+
+	int numRows;
+	int numCols;
+	int minVal;
+	int maxVal;
+
+	public RunLength(int r, int c, int min, int max){
+
+		numRows = r;
+		numCols = c;
+		minVal = min;
+		maxVal = max;
+
+
+	}
+
+}
